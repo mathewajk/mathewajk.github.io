a86ec0a1dacfd713103f026b0c46d96f49029df6
Mathew A. K <kramer.mathew.a@gmail.com>
2023-02-22 10:02:19 -0500

    Fix margins

---
+++ _nuxt/static/1677078094/cv/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1677078094",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fcv",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1ff86985"}}}}(null));
