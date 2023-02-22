bcd0ab6b1c04fa0c3a38a2f9f782bc4e1838028c
Mathew A. K <kramer.mathew.a@gmail.com>
2023-02-22 00:02:19 -0500

    Fix URL colors

---
+++ _nuxt/bc437d6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{214:function(n,o,w){w(215),n.exports=w(216)}},[[214,6,1,7]]]);
---
+++ _nuxt/static/1677042046/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1677042046",layout:"default",error:a,state:{},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"353df8e5"}}}}(null,"\u002F"));
